80773f54a2d61aacd3aba823f5e8a1553e9f47b3
kutli <kutlidev@gmail.com>
2022-09-27 15:30:59 -0500

    Init user service

--- src/main/java/com/kutli/userservice/UserServiceApplication.java
+++ src/main/java/com/kutli/userservice/UserServiceApplication.java
@@ -4,9 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@ComponentScan
 @EnableEurekaClient
 @SpringBootApplication
 public class UserServiceApplication {
